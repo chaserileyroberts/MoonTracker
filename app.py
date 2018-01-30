1450ac3656bf908aa360c4773ec27378184aee2b
qxu <qxu@github.com>
2018-01-29 19:30:58 -0500

    Simple SMS notification proto

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template, flash
+from wtforms import Form, StringField, validators
+
+from twilio.rest import Client as TwilioClient
+from coinbase.wallet.client import Client as CoinbaseClient
+from api_keys import coinbase_auth, coinbase_secret, twilio_sid, twilio_auth, app_secret
+
+coinbase_client = CoinbaseClient(coinbase_auth, coinbase_secret,
+    api_version='2017-08-07')
+
+twilio_client = TwilioClient(twilio_sid, twilio_auth)
+
+class PhoneNumberForm(Form):
+    phone_number = StringField('Phone Number', [validators.Length(min=10)])
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = PhoneNumberForm(request.form)
+    if request.method == 'POST' and form.validate():
+        phone_number = form.phone_number.data
+        price = coinbase_client.get_spot_price(currency='USD')
+        twilio_client.api.account.messages.create(
+            to=phone_number,
+            from_="+15072003597",
+            body="Bitcoin price is: " + price.amount)
+        flash("Sent to " + form.phone_number.data)
+    
+    return render_template('index.html', form=form)
+
+app.secret_key = app_secret
