1164120bc7735259fb0c4e2bf7999300a628a5d6
shanalily <shanalily@gmail.com>
2018-02-04 23:25:41 -0500

    db stuff

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, render_template, flash
-from wtforms import Form, StringField, IntegerField, validators
+from flask import Flask, request, render_template, flash, g
+from wtforms import Form, StringField, IntegerField, SelectField, validators
 
 from twilio.rest import Client as TwilioClient
 from coinbase.wallet.client import Client as CoinbaseClient
 from api_keys import coinbase_auth, coinbase_secret, twilio_sid, twilio_auth, app_secret
+
+import sqlite3
 
 coinbase_client = CoinbaseClient(coinbase_auth, coinbase_secret,
     api_version='2017-08-07')
@@ -13,8 +15,21 @@ twilio_client = TwilioClient(twilio_sid, twilio_auth)
 class PhoneNumberForm(Form):
     phone_number = StringField('Phone Number', [validators.Length(min=10)])
     target_price = IntegerField('Bitcoin Target Price', [validators.optional()])
+    less_more= SelectField('', choices=[('>='), ('<=')])
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('') # path to database
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
