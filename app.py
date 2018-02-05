1935fb3a636e2b7c5f9c5a36762f3e48ea517bbe
Sean Maltby <seanmaltby@gmail.com>
2018-02-04 22:04:04 -0500

    Add sqlite3 database, update requirements.txt. Currently, the database has a single table, alerts, which stores a ticker symbol, a target price, an above/below boolean, and a phone number. The form on the website currently inserts into this database, with the default symbol of 'BTC', and above set to true.

--- app.py
+++ app.py
@@ -5,10 +5,20 @@ from twilio.rest import Client as TwilioClient
 from coinbase.wallet.client import Client as CoinbaseClient
 from api_keys import coinbase_auth, coinbase_secret, twilio_sid, twilio_auth, app_secret
 
+import sqlite3
+
 coinbase_client = CoinbaseClient(coinbase_auth, coinbase_secret,
     api_version='2017-08-07')
 
 twilio_client = TwilioClient(twilio_sid, twilio_auth)
+
+db_conn = sqlite3.connect('moontracker_database.db')
+db_cursor = db_conn.cursor()
+
+db_cursor.execute('''CREATE TABLE IF NOT EXISTS alerts
+                (symbol text, price real, above integer, phone_number text)''')
+
+db_conn.commit()
 
 class PhoneNumberForm(Form):
     phone_number = StringField('Phone Number', [validators.Length(min=10)])
@@ -20,16 +30,22 @@ app = Flask(__name__)
 def index():
     form = PhoneNumberForm(request.form)
     if request.method == 'POST' and form.validate():
-        # need to store in database and send notification when
-        # target price is reached
         phone_number = form.phone_number.data
         target_price = form.target_price.data
-        price = coinbase_client.get_spot_price(currency='USD')
-        twilio_client.api.account.messages.create(
-            to=phone_number,
-            from_="+15072003597",
-            body="Bitcoin price is: " + price.amount)
-        flash("Sent to " + form.phone_number.data)
+
+        # Store in database
+        query = "INSERT INTO alerts VALUES ('{0}', {1}, {2}, '{3}')".format('BTC', target_price, 1, phone_number)
+        db_cursor.execute(query)
+        db_conn.commit()
+
+        # need to send notification when
+        # target price is reached
+        # price = coinbase_client.get_spot_price(currency='USD')
+        # twilio_client.api.account.messages.create(
+        #     to=phone_number,
+        #     from_="+15072003597",
+        #     body="Bitcoin price is: " + price.amount)
+        # flash("Sent to " + form.phone_number.data)
     
     return render_template('index.html', form=form)
 
