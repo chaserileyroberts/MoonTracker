dd62719f89fbe112e9f84309a67be0e2c4bf1066
shanalily <shanalily@gmail.com>
2018-02-07 23:38:07 -0500

    fixed tests

--- test_texter.py
+++ test_texter.py
@@ -15,15 +15,17 @@ class price():
 
 def test_less_than_text():
   t = twilio()
-  clients = [('555-555-5555', "1.0")]
+  clients = [('555-555-5555', '1.0', 'BTC')]
   texter.text_less_than(t.create, clients, price("3.00"))
   assert len(t.messages) == 1
   assert "below" in t.messages[0]
+  assert 'BTC' in t.messages[0]
 
 
 def test_greater_than_text():
   t = twilio()
-  clients = [('555-555-5555', "1.0")]
+  clients = [('555-555-5555', '1.0', 'BTC')]
   texter.text_greater_than(t.create, clients, price("3.00"))
   assert len(t.messages) == 1
   assert "above" in t.messages[0]
+  assert 'BTC' in t.messages[0]
