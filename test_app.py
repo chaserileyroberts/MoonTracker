b840652c48cebfab0d159864d5fb79e84079e71a
qxu <qxu@github.com>
2018-02-12 19:14:04 -0500

    flask app test case

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import flask
+import app
+
+test_client = app.app.test_client()
+
+def test_response_elems():
+    response = test_client.get('/')
+    assert response.status_code == 200
+    page = str(response.data)
+    assert "Phone Number" in page
+    assert "Coin" in page
+    assert "Target Price" in page
