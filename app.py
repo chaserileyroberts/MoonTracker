8bb4d1ff98c0ef8a2b9654a5c234ea9063d7681c
Sean Maltby <seanmaltby@gmail.com>
2018-02-14 01:32:26 -0500

    Use a scheduler for periodically checking alerts. Handle requirements in a virtual environment. Minor refactoring.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, flash, g
+from flask_apscheduler import APScheduler
 from wtforms import (Form, StringField, IntegerField,
                      SelectField, validators)
 
@@ -8,50 +9,59 @@ import sqlite3
 app = Flask(__name__)
 
 
-class WebsiteServer():
-    class PhoneNumberForm(Form):
-        phone_number = StringField(
-            'Phone Number', [
-                validators.Length(
-                    min=10), validators.Regexp(
-                    '^[0-9]+$', message="Input characters must be numeric")])
-        asset = SelectField(
-            'Coin', choices=[('BTC', 'BTC'), ('ETH', 'ETH'), ('LTC', 'LTC')])
-        target_price = IntegerField('Target Price', [validators.optional()])
-        less_more = SelectField(
-            '', choices=[(1, 'above'), (0, 'below')], coerce=int)
+def check_alerts():
+    print("CHECK ALERTS")
 
-    @staticmethod
-    def set_database(file_name):
-        WebsiteServer.db_conn = sqlite3.connect(file_name)
-        WebsiteServer.db_cursor = WebsiteServer.db_conn.cursor()
-        WebsiteServer.db_cursor.execute(
-            'CREATE TABLE IF NOT EXISTS alerts '
-            '(symbol text, price real, above integer, phone_number text)''')
-        WebsiteServer.db_conn.commit()
-        app = Flask(__name__)
-        app.secret_key = '000000'
 
-    @staticmethod
-    @app.route('/', methods=['GET', 'POST'])
-    def index():
-        form = WebsiteServer.PhoneNumberForm(request.form)
-        if request.method == 'POST' and form.validate():
-            phone_number = form.phone_number.data
-            asset = form.asset.data
-            target_price = form.target_price.data
-            less_more = form.less_more.data
+class Config(object):
+    JOBS = [
+        {
+            'id': 'job1',
+            'func': 'app:check_alerts',
+            'trigger': 'interval',
+            'seconds': 10
+        }
+    ]
 
-            # Store in database
-            cmd = "INSERT INTO alerts VALUES (?, ?, ?, ?)"
+    SCHEDULER_API_ENABLED = True
 
-            WebsiteServer.db_cursor.execute(
-                cmd, (asset, target_price, less_more, phone_number))
-            WebsiteServer.db_conn.commit()
 
-        return render_template('index.html', form=form)
+class AlertForm(Form):
+    phone_number = StringField('Phone Number', [validators.Length(min=10)])
+    asset = SelectField(
+        'Coin', choices=[('BTC', 'Bitcoin'), ('ETH', 'Ethereum'), ('LTC', 'Litecoin')])
+    target_price = IntegerField('Target Price', [validators.optional()])
+    less_more = SelectField(
+        '', choices=[(1, 'above'), (0, 'below')], coerce=int)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = AlertForm(request.form)
+    if request.method == 'POST' and form.validate():
+        phone_number = form.phone_number.data
+        asset = form.asset.data
+        target_price = form.target_price.data
+        less_more = form.less_more.data
+
+        # Store in database
+        cmd = "INSERT INTO alerts VALUES (?, ?, ?, ?)"
+
+        db_manager.db_cursor.execute(
+            cmd, (asset, target_price, less_more, phone_number))
+        db_manager.db_conn.commit()
+
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
-    WebsiteServer.set_database('moontracker_database.db')
+    app.config.from_object(Config())
+
+    db_manager = DatabaseManager("moontracker_database.db")
+
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+    scheduler.start()
+
     app.run()
+
