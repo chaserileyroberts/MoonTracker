0f92fa78867bbecbbf3111a64000af22389e241e
shanalily <shanalily@gmail.com>
2018-02-15 17:58:33 -0500

    get unit tests

--- test_app.py
+++ test_app.py
@@ -5,14 +5,12 @@ import sqlite3
 import time
 test_client = app.app.test_client()
 
-
 def setup():
     app.WebsiteServer.set_database('test.db')
 
 
 def teardown():
     os.remove('test.db')
-
 
 def test_response_elems():
     response = test_client.get('/')
@@ -41,3 +39,26 @@ def test_post_to_db():
     print(results)
     assert len(results) == 1
     assert results[0] == ('5558675309', 100.0, "BTC", 1)
+
+# def test_invalid_form_input_phone():
+#     response = test_client.post(
+#         '/',
+#         data=dict(
+#             phone_number='aaaaa',
+#             asset='BTC',
+#             less_more='1',
+#             target_price='100'
+#         ))
+#     assert response.status_code == 200
+
+# def test_invalid_form_input_price():
+#     response = test_client.post(
+#         '/',
+#         data=dict(
+#             phone_number='111111111',
+#             asset='BTC',
+#             less_more='1',
+#             target_price='aaaaa'
+#         ))
+#     assert response.status_code == 200
+#     # assert response.validate
