67a995fb6321bcb44b835dcff0cc1fb7e8e72fff
Chase Roberts <keeper6928@gmail.com>
2018-03-01 18:13:07 -0500

    Code formating.

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,65 +1,75 @@
-from app import User, db, app
-import app as webserver
+from app import db, app
 test_client = app.test_client()
 
+
 def setup():
-	app.testing = True
-	db.drop_all()
-	db.create_all()
+    app.testing = True
+    db.drop_all()
+    db.create_all()
+
 
 def teardown():
-	db.drop_all()
+    db.drop_all()
+
 
 def register(username, password, phone_number):
-	return test_client.post('/create', data=dict(
-		username=username,
-		password=password,
-		phone_number=phone_number
-	), follow_redirects=True)
+    return test_client.post('/create', data=dict(
+        username=username,
+        password=password,
+        phone_number=phone_number
+    ), follow_redirects=True)
+
 
 def login(username, password):
-	return test_client.post('/login', data=dict(
-		username=username,
-		password=password,
-	), follow_redirects=True)
+    return test_client.post('/login', data=dict(
+        username=username,
+        password=password,
+    ), follow_redirects=True)
+
 
 def logout():
-	return test_client.get('/logout', follow_redirects=True)
+    return test_client.get('/logout', follow_redirects=True)
 
 
 def test_create_login_logout():
-	response = register('test_user', '12345678', '1111111111')
-	assert response.status_code == 200
-	assert 'Successfully created new account for test_user' in str(response.data)
-	response = login('test_user', '12345678')
-	assert response.status_code == 200
-	assert 'Logged in successfully' in str(response.data)
-	response = logout()
-	assert response.status_code == 200
+    response = register('test_user', '12345678', '1111111111')
+    assert response.status_code == 200
+    assert 'Successfully created new account for test_user' in str(
+        response.data)
+    response = login('test_user', '12345678')
+    assert response.status_code == 200
+    assert 'Logged in successfully' in str(response.data)
+    response = logout()
+    assert response.status_code == 200
+
 
 def test_create_duplicate_user():
-	response = register('test_user', '12345678', '1111111111')
-	assert response.status_code == 200
-	assert 'Successfully created new account for test_user' in str(response.data)
-	response = logout()
-	assert response.status_code == 200
-	response = register('test_user', '12345678', '1111111111')
-	assert response.status_code == 200
-	assert 'Username not available' in str(response.data)
+    response = register('test_user', '12345678', '1111111111')
+    assert response.status_code == 200
+    assert 'Successfully created new account for test_user' in str(
+        response.data)
+    response = logout()
+    assert response.status_code == 200
+    response = register('test_user', '12345678', '1111111111')
+    assert response.status_code == 200
+    assert 'Username not available' in str(response.data)
+
 
 def test_empty_username():
-	response = login('', '12345678')
-	assert response.status_code == 200
-	assert 'Please enter username' in str(response.data)
+    response = login('', '12345678')
+    assert response.status_code == 200
+    assert 'Please enter username' in str(response.data)
+
 
 def test_empty_password():
-	response = login('test_user', '')
-	assert response.status_code == 200
-	assert 'Please enter password' in str(response.data)
+    response = login('test_user', '')
+    assert response.status_code == 200
+    assert 'Please enter password' in str(response.data)
+
 
 def test_invalid_account():
-	response = login('test_user', '12345678')
-	assert response.status_code == 200
-	assert 'Username or Password is invalid' in str(response.data)
+    response = login('test_user', '12345678')
+    assert response.status_code == 200
+    assert 'Username or Password is invalid' in str(response.data)
 
 # TO DO: test database
