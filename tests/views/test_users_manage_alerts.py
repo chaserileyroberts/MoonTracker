d26d8183182f248f424213dd3ce31081e90296c8
Chase Roberts <keeper6928@gmail.com>
2018-03-29 15:37:21 -0400

    pep8 fixes

--- tests/views/test_users_manage_alerts.py
+++ tests/views/test_users_manage_alerts.py
@@ -26,6 +26,7 @@ def test_correct_num_alerts():
     assert '$100' in str(response.data)
     assert '1111111111' in str(response.data)
 
+
 def test_edit():
     response = register('test_user', '12345678', '1111111111')
     assert response.status_code == 200
@@ -48,17 +49,17 @@ def test_edit():
     assert '<b>Asset:</b> Litecoin' in str(response.data)
     assert '$100' in str(response.data)
     assert '1111111111' in str(response.data)
-    response = test_client.post('/manage', 
-        data=dict(
-            phone_number="1111111111",
-            asset="BTC",
-            less_more="1",
-            target_price="100",
-            alert_id='1',
-            submit="Save Changes"
-        ),
-        follow_redirects=True)
-    assert response.status_code == 200   
+    response = test_client.post('/manage',
+                                data=dict(
+                                    phone_number="1111111111",
+                                    asset="BTC",
+                                    less_more="1",
+                                    target_price="100",
+                                    alert_id='1',
+                                    submit="Save Changes"
+                                ),
+                                follow_redirects=True)
+    assert response.status_code == 200
     response = test_client.get('/manage', follow_redirects=True)
     assert '<b>Asset:</b> Bitcoin' in str(response.data)
     assert '<b>Asset:</b> Litcoin' not in str(response.data)
@@ -86,17 +87,17 @@ def test_delete():
     assert '<b>Asset:</b> Litecoin' in str(response.data)
     assert '$100' in str(response.data)
     assert '1111111111' in str(response.data)
-    response = test_client.post('/manage', 
-        data=dict(
-            phone_number="1111111111",
-            asset="LTC",
-            less_more="1",
-            target_price="100",
-            alert_id='1',
-            submit="Delete"
-        ),
-        follow_redirects=True)
-    assert response.status_code == 200   
+    response = test_client.post('/manage',
+                                data=dict(
+                                    phone_number="1111111111",
+                                    asset="LTC",
+                                    less_more="1",
+                                    target_price="100",
+                                    alert_id='1',
+                                    submit="Delete"
+                                ),
+                                follow_redirects=True)
+    assert response.status_code == 200
     response = test_client.get('/manage', follow_redirects=True)
     assert '<b>Asset:</b> Litcoin' not in str(response.data)
     results = Alert.query.filter(Alert.user_id == 1).all()
