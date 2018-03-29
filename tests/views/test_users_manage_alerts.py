47dbc9943d1a23fd1d61a4efa083a50b838c9e3a
Chase Roberts <keeper6928@gmail.com>
2018-03-29 15:36:21 -0400

    Added manage unit tests

--- tests/views/test_users_manage_alerts.py
+++ tests/views/test_users_manage_alerts.py
@@ -62,3 +62,42 @@ def test_edit():
     response = test_client.get('/manage', follow_redirects=True)
     assert '<b>Asset:</b> Bitcoin' in str(response.data)
     assert '<b>Asset:</b> Litcoin' not in str(response.data)
+
+
+def test_delete():
+    response = register('test_user', '12345678', '1111111111')
+    assert response.status_code == 200
+
+    response = test_client.post(
+        '/',
+        data=dict(
+            phone_number='1111111111',
+            asset='LTC',
+            less_more='1',
+            target_price='100',
+            # user_id = '1'  # fake user id
+        ))
+    assert response.status_code == 200
+
+    results = Alert.query.filter(Alert.user_id == 1).all()
+    assert len(results) == 1
+
+    response = test_client.get('/manage', follow_redirects=True)
+    assert '<b>Asset:</b> Litecoin' in str(response.data)
+    assert '$100' in str(response.data)
+    assert '1111111111' in str(response.data)
+    response = test_client.post('/manage', 
+        data=dict(
+            phone_number="1111111111",
+            asset="LTC",
+            less_more="1",
+            target_price="100",
+            alert_id='1',
+            submit="Delete"
+        ),
+        follow_redirects=True)
+    assert response.status_code == 200   
+    response = test_client.get('/manage', follow_redirects=True)
+    assert '<b>Asset:</b> Litcoin' not in str(response.data)
+    results = Alert.query.filter(Alert.user_id == 1).all()
+    assert len(results) == 0
