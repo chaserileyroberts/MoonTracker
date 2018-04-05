8bdd1230b979ef89b7c97b1f04a80313d7b4fb0a
Chase Roberts <keeper6928@gmail.com>
2018-04-04 23:55:19 -0400

    Some bug fixes

--- tests/views/test_change_password.py
+++ tests/views/test_change_password.py
@@ -1,6 +1,6 @@
 from tests.utils import register, logout, login
 from tests.utils import test_client
-from moontracker.models import User
+
 
 def test_change_password():
     response = register('test_user', '12345678', '1111111111')
@@ -15,15 +15,15 @@ def test_change_password():
     assert response.status_code == 200
     response = test_client.post(
         '/settings',
-		data=dict(
-			current_password='12345678',
-			new_password='dankmemes',
-			new_password_check='dankmemes',
+        data=dict(
+            current_password='12345678',
+            new_password='dankmemes',
+            new_password_check='dankmemes',
         ),
         follow_redirects=True)
     assert response.status_code == 200
     assert "Successfully changed password" in str(response.data)
-    response =  logout()
+    response = logout()
     assert response.status_code == 200
     assert 'test_user' not in str(response.data)
     response = login('test_user', '12345678')
@@ -32,6 +32,10 @@ def test_change_password():
     response = login('test_user', 'dankmemes')
     assert response.status_code == 200
     assert 'test_user' in str(response.data)
+
+    # We have to do this to prevent test_client issues.
+    logout()
+
 
 def test_bad_password():
     response = register('test_user', '12345678', '1111111111')
@@ -46,14 +50,16 @@ def test_bad_password():
     assert response.status_code == 200
     response = test_client.post(
         '/settings',
-		data=dict(
-			current_password='thisisbad',
-			new_password='dankmemes',
-			new_password_check='dankmemes',
+        data=dict(
+            current_password='thisisbad',
+            new_password='dankmemes',
+            new_password_check='dankmemes',
         ),
         follow_redirects=True)
     assert response.status_code == 200
     assert "Current password is invalid" in str(response.data)
+    logout()
+
 
 def test_mismatched_passwords():
     response = register('test_user', '12345678', '1111111111')
@@ -68,11 +74,12 @@ def test_mismatched_passwords():
     assert response.status_code == 200
     response = test_client.post(
         '/settings',
-		data=dict(
-			current_password='12345678',
-			new_password='dankmemes',
-			new_password_check='surrealmemes',
+        data=dict(
+            current_password='12345678',
+            new_password='dankmemes',
+            new_password_check='surrealmemes',
         ),
         follow_redirects=True)
     assert response.status_code == 200
     assert "New passwords do not match" in str(response.data)
+    logout()
