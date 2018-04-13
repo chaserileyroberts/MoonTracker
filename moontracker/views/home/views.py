934d9a0b7d3a9ca08cd020fe642b984728a61afd
qxu <qxu@github.com>
2018-04-12 22:47:21 -0400

    Started percent change feature

--- moontracker/views/home/views.py
+++ moontracker/views/home/views.py
@@ -22,39 +22,26 @@ def index():
     form = AlertForm(request.form)
     if request.method == 'POST' and form.validate():
         asset = form.asset.data
-        target_price = form.target_price.data
-        less_more = form.less_more.data
+        cond_option = form.alert_cond.cond_option.data
         phone_number = form.phone_number.data
         market = form.market.data
-        had_last_price_error = False
-        if less_more == 2 or less_more == 3:
-            last_price_query = LastPrice.query.filter(
-                LastPrice.symbol == asset)
-            lp_result = last_price_query.one_or_none()
-            if lp_result is None:
-                had_last_price_error = True
-            else:
-                if less_more == 2:  # + %
-                    less_more = 1  # above
-                    target_price = lp_result.price * (
-                        target_price / 100.0 + 1.0)
-                elif less_more == 3:  # - %
-                    less_more = 0  # below
-                    target_price = lp_result.price * (
-                        1.0 - target_price / 100.0)
+        alert = Alert(
+            symbol=asset,
+            condition=cond_option,
+            phone_number=phone_number,
+            market=market)
+        if cond_option == 1 or cond_option == 0:
+            alert.price = form.alert_cond.price.data
+        elif cond_option == 2 or cond_option == 3:
+            alert.percent = form.alert_cond.percent.data
+            alert.percent_duration = form.alert_cond.percent_duration.data
 
-        if had_last_price_error:
-            flash("Internal error setting percent change!", 'error')
-        else:
-            alert = Alert(symbol=asset, price=target_price,
-                          above=less_more, phone_number=phone_number,
-                          market=market)
-            if current_user.is_authenticated:
-                alert.user_id = current_user.id
+        if current_user.is_authenticated:
+            alert.user_id = current_user.id
 
-            db.session.add(alert)
-            db.session.commit()
-            flash("Alert is set!")
+        db.session.add(alert)
+        db.session.commit()
+        flash("Alert is set!")
 
     return render_template('index.html', form=form,
                            app_markets_json=json.dumps(supported_assets))
@@ -69,15 +56,25 @@ def app_markets():
 class AlertConditionForm(Form):
     """Form for AlertConditionField."""
 
-    # cond_option_validators = [validators.AnyOf([1, 0, 2, 3])]
-    cond_option_validators = [validators.optional()]
+    cond_option_validators = [validators.AnyOf([1, 0, 2, 3])]
     cond_option = SelectField(
         'Condition Option',
-        choices=[(1, 'above'), (0, 'below'), (2, '+ %'), (3, '- %')],
+        choices=[
+            (-1, ''),
+            (1, 'Above a price'),
+            (0, 'Below a price'),
+            (2, 'Percent increase'),
+            (3, 'Percent decrease')],
         validators=cond_option_validators,
         coerce=int)
+
+    price_validators = [validators.InputRequired()]
     price = FloatField('Target Price')
+
+    percent_validators = [validators.InputRequired()]
     percent = FloatField('Target Percent Change')
+
+    percent_duration_validators = [validators.AnyOf([0, 1, 2, 3])]
     percent_duration = SelectField(
         'Target Change Duration',
         choices=[
@@ -86,6 +83,28 @@ class AlertConditionForm(Form):
             (2, '1 week'),
             (3, '1 month')],
         coerce=int)
+
+    def validate(self, extra_validators=None):
+        """Validates the AlertForm."""
+        self._errors = None
+        success = True
+        if self.cond_option.data == 1 or self.cond_option.data == 0:
+            self.price.validators = AlertConditionForm.price_validators
+            self.percent.validators = [validators.optional()]
+            self.percent_duration.validators = [validators.optional()]
+        elif self.cond_option.data == 2 or self.cond_option.data == 3:
+            self.price.validators = [validators.optional()]
+            self.percent.validators = AlertConditionForm.percent_validators
+            self.percent_duration.validators = AlertConditionForm.percent_duration_validators
+
+        for name, field in self._fields.items():
+            if extra_validators is not None and name in extra_validators:
+                extra = extra_validators[name]
+            else:
+                extra = tuple()
+            if not field.validate(self, extra):
+                success = False
+        return success
 
 
 class AlertConditionWidget(widgets.TableWidget):
@@ -142,16 +161,7 @@ class AlertForm(Form):
     asset = SelectField(
         'Coin', choices=assets)
 
-    target_price = FloatField('Target Price', [validators.optional()])
-
-    less_more_validators = [validators.AnyOf([1, 0, 2, 3])]
-    less_more = SelectField(
-        '',
-        choices=[(1, 'above'), (0, 'below'), (2, '+ %'), (3, '- %')],
-        validators=less_more_validators,
-        coerce=int)
-
-    # alert_cond = AlertConditionField('Alert Condition')
+    alert_cond = AlertConditionField('Alert Condition')
 
     recaptcha = RecaptchaField(
         'Recaptcha', validators=[
