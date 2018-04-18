372861fc169ecb30c07d73073dd81872c17c869d
Chase Roberts <keeper6928@gmail.com>
2018-04-18 16:23:23 -0400

    Added http -> https redirect

---
+++ moontracker/app_redirect.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
+
+app.run("0.0.0.0", port=5050)
