fe3b570273ebf0349ea9cc5eb57c68d4e61695e3
Sean Maltby <seanmaltby@gmail.com>
2018-04-19 17:21:55 -0400

    Add sanity checks to user model

---
+++ tests/test_models.py
@@ -0,0 +1,9 @@
+from moontracker.models import User
+
+
+def test_sanity():
+	user = User("test", "test", "1111111111")
+
+	assert user.is_authenticated()
+	assert user.is_active()
+	assert not user.is_anonymous()
