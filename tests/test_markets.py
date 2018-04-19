4ce1f97c98dde93c4f775a08433b959c3f9c1dda
Sean Maltby <seanmaltby@gmail.com>
2018-04-19 17:21:39 -0400

    Improve code coverage of markets

--- tests/test_markets.py
+++ tests/test_markets.py
@@ -1,5 +1,7 @@
-from moontracker.markets import (Market,
+from moontracker.markets import (Market, CoinbaseMarket, NasdaqMarket,
                                  GdaxMarket, GeminiMarket, lookupMarket)
+import pytest
+from datetime import datetime, timedelta
 
 
 def test_all_markets_sanity():
@@ -15,3 +17,25 @@ def test_lookupMarket():
     for m in markets:
         mrkt = lookupMarket(m)
         assert issubclass(mrkt, Market)
+
+
+def test_not_implemented_error():
+    market = Market()
+
+    with pytest.raises(NotImplementedError) as err:
+        market.get_spot_price('BTC')
+
+
+def test_runtime_error():
+    market = Market()
+
+    with pytest.raises(RuntimeError) as err:
+        market._handle_request('https://api.gdax.com/products/FAKE')
+
+
+def test_value_errors():
+    markets = [CoinbaseMarket, GdaxMarket, GeminiMarket, NasdaqMarket]
+    for m in markets:
+        mrkt = m()
+        with pytest.raises(ValueError) as err:
+            mrkt.get_spot_price('BTC', datetime.utcnow() - timedelta(days=40))
